7f6a6b4a824afbb1daf00ddf9c31aa2fed7141d6
Otoniel <252310356@itslerdo.edu.mx>
2026-08-17 13:30:11 -0600

    actividad 1.4

---
+++ Corte 1/act14.php
@@ -0,0 +1,8 @@
+<?php
+$nombre = "Juan";
+$edad = 30;
+$personas = ["Ana", "Luis", "Carlos"];
+
+echo memory_get_usage();  // Returns current memory usage
+echo strlen($nombre);      // If you want memory of the variable
+?>
---
+++ Corte 1/act14.py
@@ -0,0 +1,7 @@
+import sys
+
+nombre = "Juan"
+edad = 30
+personas = ["Ana", "Luis", "Carlos"]
+
+print(sys.getsizeof(nombre))  # Gets size of the variable in bytes
