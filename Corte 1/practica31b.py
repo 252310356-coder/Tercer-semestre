310b8115ecb3f54d150509f1da929e2a324f0d29
Otoniel <252310356@itslerdo.edu.mx>
2026-09-18 19:49:27 -0600

    Listo

--- Corte 1/practica31b.py
+++ Corte 1/practica31b.py
@@ -12,4 +12,28 @@ while True:
         break
     pila.append(elemento)
 
+pila_invertida = [ ]
+while pila:
+    pila_invertida.append(pila.pop())
+
 print("Pila después de apilar elementos:", pila)
+print("Pila invertida:", pila_invertida)
+
+#Programa para verificar si una palabra es un palindromo
+# Solicitar al usuario que ingrese una palabra
+palabra = input("Ingrese una palabra: ")
+# Extraer cada caracter de la palabra y apilarlo en otra pila
+pila_caracteres = deque()
+for caracter in palabra:
+    pila_caracteres.append(caracter)
+
+# Formar la nueva palabra a partir de los caracteres apilados
+palabra_invertida = ""
+while pila_caracteres:
+    palabra_invertida += pila_caracteres.pop()
+
+# Comparar la palabra original con la nueva palabra formada
+if palabra == palabra_invertida:
+    print("La palabra es un palíndromo.")
+else:
+    print("La palabra no es un palíndromo.")
